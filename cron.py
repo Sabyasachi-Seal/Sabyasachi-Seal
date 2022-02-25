7545ac8993f59024fa886ad44b116481da7d6ab2
Sabyasachi_Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-02-25 18:08:56 +0530

    Cron Job Update

--- cron.py
+++ cron.py
@@ -3,31 +3,16 @@ import random
 import yaml
 import os
 
-# readmeraw = "https://raw.githubusercontent.com/Sabyasachi-Seal/Sabyasachi-Seal/main/README.md"
-# cronjobraw = "https://raw.githubusercontent.com/Sabyasachi-Seal/Sabyasachi-Seal/main/.github/workflows/Profile3D.yml"
-# readme = requests.get(readmeraw)
-# cron = requests.get(cronjobraw)
-# readmecontent = readme.text
-# croncontent = cron.text
-# cron = croncontent[croncontent.index("- cron"): croncontent.index("- cron") + 21]
+cronjobraw = "https://raw.githubusercontent.com/Sabyasachi-Seal/Sabyasachi-Seal/main/.github/workflows/Profile3D.yml"
+cron = requests.get(cronjobraw)
+croncontent = cron.text
+croncontent = croncontent.split("\n")
+croncontent[3] =   f"  - cron: '0 */{str(random.randint(1, 8))} * * *'"
 
-yamlfile =  open('.github\workflows\Profile3D.yml', 'r')
-fullfile = yaml.load(yamlfile, Loader=yaml.FullLoader)
-yamlfile.close()
-print(fullfile)
+yamlfile =  open('.github\workflows\Profile3D2.yml', 'w')
 
-cron = fullfile[True]["schedule"][-1]["cron"]
+yamlfile.write("\n".join(croncontent))
 
-# print(cron)
-
-cron = "'0 */" + str(random.randint(1, 8)) + " * * *'"
-
-fullfile[True]["schedule"][-1]["cron"] = cron
-
-# print(fullfile)
-
-yamlfile =  open('.github\workflows\Profile3D.yml', 'w')
-yaml.dump(fullfile, yamlfile)
 yamlfile.close()
 
 print("Updated Cron Job")
