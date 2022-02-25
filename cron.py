3927b95a1d09aa1c2ee2a8aa31ccb2c4d5d8fe24
Sabyasachi_Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-02-25 17:14:22 +0530

    Added Cron Automater

---
+++ cron.py
@@ -0,0 +1,42 @@
+import requests
+import random
+import yaml
+import os
+
+# readmeraw = "https://raw.githubusercontent.com/Sabyasachi-Seal/Sabyasachi-Seal/main/README.md"
+
+# cronjobraw = "https://raw.githubusercontent.com/Sabyasachi-Seal/Sabyasachi-Seal/main/.github/workflows/Profile3D.yml"
+
+# readme = requests.get(readmeraw)
+
+# cron = requests.get(cronjobraw)
+
+# readmecontent = readme.text
+
+# croncontent = cron.text
+
+# cron = croncontent[croncontent.index("- cron"): croncontent.index("- cron") + 21]
+
+yamlfile =  open('Sabyasachi-Seal\.github\workflows\Profile3D.yml', 'r')
+fullfile = yaml.load(yamlfile, Loader=yaml.FullLoader)
+yamlfile.close()
+
+cron = fullfile[True]["schedule"][-1]["cron"]
+
+# print(cron)
+
+cron = "0 */" + str(random.randint(1, 8)) + " * * *"
+
+fullfile[True]["schedule"][-1]["cron"] = cron
+
+# print(fullfile)
+
+yamlfile =  open('Sabyasachi-Seal\.github\workflows\Profile3D.yml', 'w')
+yaml.dump(fullfile, yamlfile)
+yamlfile.close()
+
+print("Updated Cron Job")
+
+os.system('git add .')
+os.system('git commit -m "Cron Job Update"')
+os.system('git push')
