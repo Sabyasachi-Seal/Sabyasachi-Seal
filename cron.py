d2b54e91b23509c9285e65e31e41308634643785
Sabyasachi Seal <yoboy907@gmail.com>
2022-02-28 17:27:13 +0530

    Fixed Repeated Random

--- cron.py
+++ cron.py
@@ -1,13 +1,20 @@
 import requests
 import random
-import yaml
-import os
+# import yaml
+# import os
 
 cronjobraw = "https://raw.githubusercontent.com/Sabyasachi-Seal/Sabyasachi-Seal/main/.github/workflows/Profile3D.yml"
 cron = requests.get(cronjobraw)
 croncontent = cron.text
 croncontent = croncontent.split("\n")
-croncontent[3] =   f'  - cron: "0 */{str(random.randint(1, 8))} * * *"'
+
+oldcron = croncontent[3]
+newcron =   f'  - cron: "0 */{str(random.randint(1, 8))} * * *"'
+
+while (oldcron == newcron):
+    newcron =  f'  - cron: "0 */{str(random.randint(1, 8))} * * *"'
+
+croncontent[3] = newcron
 
 yamlfile =  open('.github/workflows/Profile3D.yml', 'w')
 
